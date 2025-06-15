d0ba4735d8bbd9041e115e06714eccf1d5543a08
hailia sommerville <hailiassommerville@gmail.com>
2025-06-15 18:07:36 -0400

    Complete problem 12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,18 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  let fb = []
+  
+  for (let i = start; i <= end; i++){
+    if (i % 3 === 0 && i % 5 === 0)
+      fb.push("FizzBuzz")
+    else if (i % 3 === 0) 
+      fb.push("Fizz")
+    else if (i % 5 === 0 )
+      fb.push("Buzz")
+    else
+    fb.push(i)
+    
+  }
+  return fb
 }
 
 // Do not edit this line;
