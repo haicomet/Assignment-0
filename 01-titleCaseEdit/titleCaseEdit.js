30df54c8883f519e00b396b1a20fb517f7374c8f
hailia sommerville <hailiassommerville@gmail.com>
2025-06-15 14:11:07 -0400

    Complete problem 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,12 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  let words = title.split(' ')
+  let titleCase = []
+  for (let i = 0; i < words.length; i++){
+    let word = words[i]
+    let capitalized = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    titleCase.push(capitalized)
+  }
+  return titleCase.join(' ')
 }
 
 // Do not edit this line;
