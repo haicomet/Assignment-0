ff65dc5dad46f42b1a840c62ee06c35beebd5a17
hailia sommerville <hailiassommerville@gmail.com>
2025-06-15 18:17:44 -0400

    Complete problem 13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,9 @@
 function isPalindrome(word) {
-  // Insert code here;
+  let reversedWord = ""
+  for (let i = word.length - 1; i >= 0; i--)
+    reversedWord += word[i] 
+
+  return word === reversedWord
 }
 
 // Do not edit this line;
