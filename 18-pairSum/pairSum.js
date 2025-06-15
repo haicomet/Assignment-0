c10ee8718d66df8b1e07428c4385f4ce05a06165
hailia sommerville <hailiassommerville@gmail.com>
2025-06-15 19:44:44 -0400

    Complete problem 18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,13 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  if (nums.length <= 1)
+    throw new Error("Array must be >1!")
+  for (let i = 0; i < nums.length; i++){
+    for (let j =  i + 1; j < nums.length; j++){
+      if (nums[i] + nums[j] === target)
+        return true
+    }
+  }
+  return false
 }
 
 // Do not edit this line;
