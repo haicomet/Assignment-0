3c96594e448a4f0b6ce7ba64265e64ce7bcde624
hailia sommerville <hailiassommerville@gmail.com>
2025-06-15 18:57:02 -0400

    Complete problem 15

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,12 @@
 class MySolution {
   countDownSum(num) {
-    // Insert code here;
+    if (num <= 0)
+      return 0
+    if (num === 1)
+      return 1
+
+    return num + this.countDownSum(num - 1)
+
   }
 }
 
